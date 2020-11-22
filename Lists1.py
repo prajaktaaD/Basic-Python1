281dba666ae55e4ef575a478862f6c3ba76432da
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-22 14:51:52 +0530

    Create Lists1.py
    Basic Start with python and bit of generating lists

---
+++ Lists1.py
@@ -0,0 +1,16 @@
+print ('Hello World')
+x=y=z=7
+print(x,y,z)
+z=10
+z='Hello'
+print(z)
+a,b,c=10,10.5,10+5j
+print(type(a),type(b),type(c))
+a=True
+b=False
+print(type(a),type(b))
+list2=[]
+list2=[1,'a','apple']
+print(list2)
+list2[2]=33
+print(list2)
